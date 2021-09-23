5081cc978403595ceaca3e475ba86f074f097664
init-zero2023 <santosh.sk11112222@gmail.com>
2021-09-23 20:11:42 +0530

    prototype ready

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ const express = require("express")
 const path = require("path")
 const bodyParser = require("body-parser")
 const fs = require("fs")
+require('dotenv').config()
+const accountSid = process.env.Sid
+const authToken = process.env.Token
+const client = require('twilio')(accountSid, authToken)
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 const publicPath = path.join(__dirname, "public")
@@ -16,46 +21,54 @@ app.get("/", (req, res)=>{
 })
 
 app.post('/', (req, res)=>{
-    console.log(req.body.message)
+    var data = fs.readFileSync("contacts.json")
+    var myObject = JSON.parse(data)
+    for(const id in myObject){
+        console.log(myObject[id].mobile)
+        client.messages 
+        .create({ 
+            body:req.body.message, 
+            from: 'whatsapp:+14155238886',       
+            to: `whatsapp:${myObject[id].mobile}` 
+        }) 
+        .then(message => console.log(message.sid, message.errorMessage)) 
+        .done()
+    }
     res.redirect('/')
 })
 
 app.post("/addcontact", (req, res)=>{
-   
-    // var data = fs.readFileSync("contacts.json")
-    // var myObject = JSON.parse(data)
-    // console.log(myObject)
-    // console.log(typeof(myObject))
-
-    // let currentdata = {
-    //     mobile: req.body.contact
-    // }
-
-    // // myObject.push(currentdata)
-
-    // var newData = JSON.stringify(myObject);
-    // fs.writeFile("contacts.json", newData, (err) => {
-    // // Error checking
-    // if (err) throw err;
-    // console.log("New data added");
-    // });
-    // console.log(req.body.contact)
-
-
-    fs.readFile('contacts.json', 'utf8', function readFileCallback(err, data){
+    fs.readFile('contacts.json', 'utf8', function readFileCallback(err, olddata){
         if (err){
             console.log(err);
         } else {
-        obj = JSON.parse(data); //now it an object
-        console.log(Object.entries(obj))
+            let data
+            let  id = uuidv4().toString()
+            if(olddata === ''){
+                console.log("empty object")
+                
+                let obj = {}
+                obj[id] = {
+                    mobile: req.body.contact
+                }
+                data = obj
+            }else{
+                olddataJson = JSON.parse(olddata)
+                olddataJson[id] = {
+                    mobile: req.body.contact
+                }
+                data = olddataJson
+            }
         // obj.push({mobile: req.body.contact}); //add some data
-        json = JSON.stringify(obj); //convert it back to json
-        fs.writeFile('contacts.json', json, 'utf8', callback); // write it back 
+        json = JSON.stringify(data); //convert it back to json
+        fs.writeFile('contacts.json', json, 'utf8', (something)=>{
+            console.log(something)
+        }); // write it back 
     }});
 
     res.redirect("/")
 })
 
-app.listen(3000, ()=>{
-    console.log("Server is starting at port 3000")
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is starting at ${process.env.PORT}`)
 })
